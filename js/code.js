79f628fa7e913da85758c536076d43390ba8ce9e
alegonzalez <aoalvaradog@est.utn.ac.cr>
2017-01-03 20:12:45 -0600

    validación horizontal

--- js/code.js
+++ js/code.js
@@ -1,6 +1,13 @@
 $(document).ready(function(){
   var row = 0;
   var turn = "";
+  var player_1="";
+  var player_2="";
+  //red
+
+  position_player2 = [];
+  //green
+  position_player1 = [];
   $('.btn').attr("disabled", true);
   $('.start').click(function(){
     validateAndStart($('.player_1').val(),$('.player_2').val());
@@ -40,21 +47,28 @@ $(document).ready(function(){
 function colourDiv(row){
   var cont = 0;
   var color = "";
-  debugger;
   for (var i = 0; i < 6; i++){
     if($("."+row +"_"+ i+"").css("background-color") != "rgb(255, 255, 255)" ){
       cont = i - 1;
       color = (turn == 0) ? "green" : "red";
+      result = (turn == 1) ? player_1 : player_2;
+      $('#playerTurn').text("El turno es para " + result);
       putColor(color,cont,row);
+      getPosition(player_1,player_2);
       break;
     }else if(i == 5){
       color = (turn == 0) ? "green" : "red";
+      result = (turn == 1) ? player_1 : player_2;
+      $('#playerTurn').text("El turno es para " + result);
       putColor(color,i,row);
+      getPosition(player_1,player_2);
     }
   }
 }
 function putColor(color,i,row){
   $("."+row +"_"+ i +"").css("background",color);
+  color = (color == "green") ? "red" : "green";
+  $("#image").css("background", color);
   turn = (turn == 0) ? 1 : 0;
 }
 //load button in the panel
@@ -81,6 +95,8 @@ function validateAndStart(player1,player2){
 
 //Player Turn start
 function playerTurn(player1,player2){
+  player_1 = player1;
+  player_2 = player2;
   var number = Math.floor((Math.random() * 2));
   result = (number == 1) ? player2 : player1;
   $('#playerTurn').text("El turno es para " + result);
@@ -88,11 +104,66 @@ function playerTurn(player1,player2){
 }
 //show image
 function showimage(right){
+  if(turn == 1){
+    $("#image").css({
+      "position":"relative",
+      "right": right+"px",
+      "background": "red"
+    });
+  }else{
+    $("#image").css({
+      "position":"relative",
+      "right": right+"px",
+    });
+  }
   $("#image").show();
-  $("#image").css({
-    "position":"relative",
-    "right": right+"px",
-  });
   $("hr").css("top","-5px");
   $(".btn").css("top","-3px");
+}
+//get position
+function getPosition(player_1,player_2){
+  for (var i = 0; i < 7; i++) {
+    for (var j = 0; j < 6; j++) {
+      if($('.'+i+'_'+j).css("background-color") == "rgb(255, 0, 0)"){
+        position_player2.push(i+"-"+j);
+      }else if ($('.'+i+'_'+j).css("background-color") == "rgb(0, 128, 0)") {
+        position_player1.push(i+"-"+j);
+
+      }
+    }
+  }
+  validateHorizontal(position_player2,player_2);
+  validateHorizontal(position_player1,player_1);
+  position_player1 = [];
+  position_player2 = [];
+}
+function validateHorizontal(position,namePlayer){
+var cont = 0;
+var last = [];
+for (var i = 0; i < position.length; i++) {
+  result = position[i].split("-");
+  position[i] = result[1] + "-" + result[0];
+}
+position = position.sort();
+for (var i = 0; i < position.length; i++) {
+  result = position[i].split("-");
+  position[i] = result[1] + "-" + result[0];
+  result = position[i].split("-");
+  if(i != 0){
+    if(result[0] -1 == last[0] && result[1] == last[1] ){
+      cont ++;
+    }else{
+      cont = 0;
+    }
+  }else{
+    cont ++;
+  }
+  if(cont == 4){
+    alert("Gano el jugador " + namePlayer);
+    $("#image").hide(1000);
+    break;
+  }
+  last[0] = parseInt(result[0]);
+  last[1] = parseInt(result[1]);
+}
 }
