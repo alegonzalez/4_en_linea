4dfc95fe6969b8cbdfcff5a42797cb1dedce7d55
alegonzalez <aoalvaradog@est.utn.ac.cr>
2017-01-10 13:15:16 -0600

    Finalización del 4 en linea

--- js/code.js
+++ js/code.js
@@ -3,17 +3,16 @@ $(document).ready(function(){
   var turn = "";
   var player_1="";
   var player_2="";
-  //red
-
   position_player2 = [];
-  //green
   position_player1 = [];
-  $('.btn').attr("disabled", true);
+  $('.btnGame').attr("disabled", true);
   $('.start').click(function(){
     validateAndStart($('.player_1').val(),$('.player_2').val());
   })
   $('.panel').hide();
   $("#image").hide();
+  $('.gamebutton').hide();
+  //event hover
   $('.panel').mousemove(function(event) {
     var left = event.pageX - $(this).offset().left;
     var top = event.pageY - $(this).offset().top;
@@ -43,7 +42,23 @@ $(document).ready(function(){
   $('.panel').click(function(){
     colourDiv(row);
   });
+  $('#startWithDifferentPlayer').click(function(){
+    $('.player_1,.player_2').val("");
+    $('.player_1,.player_2,.start').show(1000);
+    $('#playerTurn').hide(1000);
+    $(".panel").hide(1000);
+    $('#startWithDifferentPlayer').hide(1000);
+    $('#startWithSamePlayer').hide(1000);
+  });
+  $("#startWithSamePlayer").click(function(){
+    $('.panel').empty();
+    loadPanel();
+    $('.panel').show(1000);
+    $('#playerTurn').show();
+
+  });
 });
+//This function call function for paint, and call the method  with name getPosition for validations
 function colourDiv(row){
   var cont = 0;
   var color = "";
@@ -60,11 +75,12 @@ function colourDiv(row){
       color = (turn == 0) ? "green" : "red";
       result = (turn == 1) ? player_1 : player_2;
       $('#playerTurn').text("El turno es para " + result);
-      putColor(color,i,row);
       getPosition(player_1,player_2);
+      putColor(color,i,row);
     }
   }
 }
+//This function put color in the panel
 function putColor(color,i,row){
   $("."+row +"_"+ i +"").css("background",color);
   color = (color == "green") ? "red" : "green";
@@ -74,13 +90,16 @@ function putColor(color,i,row){
 //load button in the panel
 function loadPanel(){
   var button = "";
+  $(".panel").append("<center id='tagCenterOfImage'><div id='image'></div></center>");
   $(".panel").append("<hr>");
   for (var i = 0; i < 6; i++) {
     for (var j = 0; j < 7; j++) {
-      button += "<div   class='btn "+j+"_"+i+"'></div>";
+      button += "<div   class='btnGame "+j+"_"+i+"'></div>";
     }
     $(".panel").append(button);
     button = "<br>";
+    $(".btnGame").removeAttr("style");
+    $("hr").removeAttr("style");
   }
 }
 //get name, validate the name and start game
@@ -89,7 +108,10 @@ function validateAndStart(player1,player2){
     loadPanel();
     $('.panel').show(1000);
     playerTurn(player1,player2);
+    $('#playerTurn').show(1000);
     $('.player_1,.player_2,.start').hide(1000);
+  }else{
+    alert("Los nombres de jugadores son requeridos");
   }
 }
 
@@ -118,7 +140,7 @@ function showimage(right){
   }
   $("#image").show();
   $("hr").css("top","-5px");
-  $(".btn").css("top","-3px");
+  $(".btnGame").css("top","-3px");
 }
 //get position
 function getPosition(player_1,player_2){
@@ -128,17 +150,27 @@ function getPosition(player_1,player_2){
         position_player2.push(i+"-"+j);
       }else if ($('.'+i+'_'+j).css("background-color") == "rgb(0, 128, 0)") {
         position_player1.push(i+"-"+j);
-
       }
     }
   }
   if(position_player1.length != 0 && position_player2.length != 0){
-    validateHorizontal(position_player2,player_2);
-    validateHorizontal(position_player1,player_1);
-    validateVertical(position_player2,player_2);
-    validateVertical(position_player1,player_1);
+    result = validateHorizontal(position_player2,player_2);
+    if(result == false){
+      result = validateHorizontal(position_player1,player_1);
+      if(result == false){
+        result = validateVertical(position_player2,player_2);
+        if(result == false){
+          result = validateVertical(position_player1,player_1);
+          if(result == false){
+            result = validateDiagonal(position_player1,player_1);
+            if(result == false){
+              validateDiagonal(position_player2,player_2);
+            }
+          }
+        }
+      }
+    }
   }
-
   position_player1 = [];
   position_player2 = [];
 }
@@ -163,19 +195,15 @@ function validateHorizontal(position,namePlayer){
     }else{
       cont ++;
     }
-    if(cont == 4){
-      alert("Gano el jugador " + namePlayer);
-      $("#image").hide(1000);
-      break;
-    }
+    if(gameWon(cont,namePlayer) == true)
+    return true;
     last[0] = parseInt(result[0]);
     last[1] = parseInt(result[1]);
   }
+  return false;
 }
 //this function validate vertical
 function validateVertical(position,namePlayer){
-  debugger;
-
   var array = position[0].split("-");
   var beforeLeft = parseInt(array[0]);
   var beforeRight = parseInt(array[1]);
@@ -188,13 +216,155 @@ function validateVertical(position,namePlayer){
     }else{
       cont = 0;
     }
-
-    if(cont == 4){
-      alert("Gano el jugador " + namePlayer);
-      $("#image").hide(1000);
-      break;
-
-    }
+    if(gameWon(cont,namePlayer) == true)
+    return true;
   }
-  console.log(position);
+  return false;
+}
+//this function validate if player won the game
+function gameWon(cont,namePlayer){
+  if(cont == 4){
+    alert("El ganador es " + namePlayer);
+    clean();
+    $('.gamebutton').show();
+    return true;
+  }else{
+    return false;
+  }
+}
+//this function clean game
+function clean(){
+  $("#tagCenterOfImage").remove();
+  $(".panel").hide(1000);
+  $(".btnGame").hide(1000);
+  $(".btnGame").remove();
+  $('#playerTurn').hide(1000);
+  $("hr").remove();
+  $("br").remove();
+  position_player2 = [];
+  position_player1 = [];
+}
+function validateDiagonal(position,namePlayer){
+  if(position.indexOf("0-5") != -1 && position.indexOf("1-4") != -1 && position.indexOf("2-3") != -1 && position.indexOf("3-2") != -1){
+    alert("Gano el jugador " + namePlayer);
+    $('.gamebutton').show();
+    clean();
+    return true;
+  }else if (position.indexOf("0-4") != -1 && position.indexOf("1-3") != -1 && position.indexOf("2-2") != -1 && position.indexOf("3-1") != -1) {
+    alert("Gano el jugador " + namePlayer);
+    $('.gamebutton').show();
+    clean();
+    return true;
+  }else if (position.indexOf("0-3") != -1 && position.indexOf("1-2") != -1 && position.indexOf("2-1") != -1 && position.indexOf("3-0") != -1) {
+    alert("Gano el jugador " + namePlayer);
+    $('.gamebutton').show();
+    clean();
+    return true;
+  }else if (position.indexOf("1-5") != -1 && position.indexOf("2-4") != -1 && position.indexOf("3-3") != -1 && position.indexOf("4-2") != -1) {
+    alert("Gano el jugador " + namePlayer);
+    $('.gamebutton').show();
+    clean();
+    return true;
+  }else if (position.indexOf("1-4") != -1 && position.indexOf("2-3") != -1 && position.indexOf("3-2") != -1 && position.indexOf("4-1") != -1) {
+    alert("Gano el jugador " + namePlayer);
+    $('.gamebutton').show();
+    clean();
+    return true;
+  }else if (position.indexOf("1-3") != -1 && position.indexOf("2-2") != -1 && position.indexOf("3-1") != -1 && position.indexOf("4-0") != -1) {
+    alert("Gano el jugador " + namePlayer);
+    $('.gamebutton').show();
+    clean();
+    return true;
+  }else if (position.indexOf("2-5") != -1 && position.indexOf("3-4") != -1 && position.indexOf("4-3") != -1 && position.indexOf("5-2") != -1) {
+    alert("Gano el jugador " + namePlayer);
+    $('.gamebutton').show();
+    clean();
+    return true;
+  }else if (position.indexOf("2-4") != -1 && position.indexOf("3-3") != -1 && position.indexOf("4-2") != -1 && position.indexOf("5-1") != -1) {
+    alert("Gano el jugador " + namePlayer);
+    $('.gamebutton').show();
+    clean();
+    return true;
+  }else if (position.indexOf("2-3") != -1 && position.indexOf("3-2") != -1 && position.indexOf("4-1") != -1 && position.indexOf("5-0") != -1) {
+    alert("Gano el jugador " + namePlayer);
+    $('.gamebutton').show();
+    clean();
+    return true;
+  }else if (position.indexOf("3-5") != -1 && position.indexOf("4-4") != -1 && position.indexOf("5-3") != -1 && position.indexOf("6-2") != -1) {
+    alert("Gano el jugador " + namePlayer);
+    $('.gamebutton').show();
+    clean();
+    return true;
+  }else if (position.indexOf("3-4") != -1 && position.indexOf("4-3") != -1 && position.indexOf("5-2") != -1 && position.indexOf("6-1") != -1) {
+    alert("Gano el jugador " + namePlayer);
+    $('.gamebutton').show();
+    clean();
+    return true;
+  }else if (position.indexOf("3-3") != -1 && position.indexOf("4-2") != -1 && position.indexOf("5-1") != -1 && position.indexOf("6-0") != -1) {
+    alert("Gano el jugador " + namePlayer);
+    $('.gamebutton').show();
+    clean();
+    return true;
+  }else if (position.indexOf("6-5") != -1 && position.indexOf("5-4") != -1 && position.indexOf("4-3") != -1 && position.indexOf("3-2") != -1) {
+    alert("Gano el jugador " + namePlayer);
+    $('.gamebutton').show();
+    clean();
+    return true;
+  }else if (position.indexOf("6-4") != -1 && position.indexOf("5-3") != -1 && position.indexOf("4-2") != -1 && position.indexOf("3-1") != -1) {
+    alert("Gano el jugador " + namePlayer);
+    $('.gamebutton').show();
+    clean();
+    return true;
+  }else if (position.indexOf("6-3") != -1 && position.indexOf("5-2") != -1 && position.indexOf("4-1") != -1 && position.indexOf("3-0") != -1) {
+    alert("Gano el jugador " + namePlayer);
+    $('.gamebutton').show();
+    clean();
+    return true;
+  }else if (position.indexOf("5-5") != -1 && position.indexOf("4-4") != -1 && position.indexOf("3-3") != -1 && position.indexOf("2-2") != -1) {
+    alert("Gano el jugador " + namePlayer);
+    $('.gamebutton').show();
+    clean();
+    return true;
+  }else if (position.indexOf("5-4") != -1 && position.indexOf("4-3") != -1 && position.indexOf("3-2") != -1 && position.indexOf("2-1") != -1) {
+    alert("Gano el jugador " + namePlayer);
+    $('.gamebutton').show();
+    clean();
+    return true;
+  }else if (position.indexOf("5-3") != -1 && position.indexOf("4-2") != -1 && position.indexOf("3-1") != -1 && position.indexOf("2-0") != -1) {
+    alert("Gano el jugador " + namePlayer);
+    $('.gamebutton').show();
+    clean();
+    return true;
+  }else if (position.indexOf("4-5") != -1 && position.indexOf("3-4") != -1 && position.indexOf("2-3") != -1 && position.indexOf("1-2") != -1) {
+    alert("Gano el jugador " + namePlayer);
+    $('.gamebutton').show();
+    clean();
+    return true;
+  }else if (position.indexOf("4-4") != -1 && position.indexOf("3-3") != -1 && position.indexOf("2-2") != -1 && position.indexOf("1-1") != -1) {
+    alert("Gano el jugador " + namePlayer);
+    $('.gamebutton').show();
+    clean();
+    return true;
+  }else if (position.indexOf("4-3") != -1 && position.indexOf("3-2") != -1 && position.indexOf("2-1") != -1 && position.indexOf("1-0") != -1) {
+    alert("Gano el jugador " + namePlayer);
+    $('.gamebutton').show();
+    clean();
+    return true;
+  }else if (position.indexOf("3-5") != -1 && position.indexOf("2-4") != -1 && position.indexOf("1-3") != -1 && position.indexOf("0-2") != -1) {
+    alert("Gano el jugador " + namePlayer);
+    $('.gamebutton').show();
+    clean();
+    return true;
+  }else if (position.indexOf("3-4") != -1 && position.indexOf("2-3") != -1 && position.indexOf("1-2") != -1 && position.indexOf("0-1") != -1) {
+    alert("Gano el jugador " + namePlayer);
+    $('.gamebutton').show();
+    clean();
+    return true;
+  }else if (position.indexOf("3-3") != -1 && position.indexOf("2-2") != -1 && position.indexOf("1-1") != -1 && position.indexOf("0-0") != -1) {
+    alert("Gano el jugador " + namePlayer);
+    $('.gamebutton').show();
+    clean();
+    return true;
+  }
+  return false;
 }
